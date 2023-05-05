03a536bb3cdc6d27c00e489f1346879daf3d70b9
ezoushen <ezoushen@gmail.com>
2023-05-05 17:59:58 +0800

    feat: support text-decoration

--- Sources/HTMLAttributedStringParser/HTMLAttributeRenderers/StyleAttributeRenderer.swift
+++ Sources/HTMLAttributedStringParser/HTMLAttributeRenderers/StyleAttributeRenderer.swift
@@ -62,6 +62,8 @@ open class StyleAttributeParser: HTMLAttributeRenderer {
                 guard let alignment = parseTextAlign(value) else { continue }
                 paragraphStyleMutated = true
                 paragraphStyle.alignment = alignment
+            case "text-decoration":
+                parseTextDecoration(value, attributes: &attributes)
             default:
                 continue
             }
@@ -112,6 +114,72 @@ open class StyleAttributeParser: HTMLAttributeRenderer {
         case "right": return .right
         case "center": return .center
         default: return nil
+        }
+    }
+
+    private func parseTextDecoration(
+        _ value: String,
+        attributes: inout [NSAttributedString.Key: Any])
+    {
+        var pendingStyle: NSUnderlineStyle?
+        var pendingColor: Color?
+
+        func applyLine(
+            _ style: NSAttributedString.Key,
+            _ color: NSAttributedString.Key,
+            attributes: inout [NSAttributedString.Key: Any])
+        {
+            attributes[style] = pendingStyle?.union(.single).rawValue ?? NSUnderlineStyle.single.rawValue
+            guard let pendingColor = pendingColor else { return }
+            attributes[color] = pendingColor
+        }
+
+        func applyStyle(_ style: NSUnderlineStyle, attributes: inout [NSAttributedString.Key: Any]) {
+            let currentUnderline = attributes[.underlineStyle] as? Int
+            let currentStrikethrough = attributes[.strikethroughStyle] as? Int
+            if let currentUnderline {
+                attributes[.underlineStyle] =
+                    NSUnderlineStyle(rawValue: currentUnderline).union(style).rawValue
+            }
+            if let currentStrikethrough {
+                attributes[.strikethroughStyle] =
+                    NSUnderlineStyle(rawValue: currentStrikethrough).union(style).rawValue
+            }
+            if currentUnderline == nil && currentStrikethrough == nil {
+                pendingStyle = style
+            }
+        }
+
+        func applyColor(_ color: Color, attributes: inout [NSAttributedString.Key: Any]) {
+            let containsUnderline = attributes.keys.contains(.underlineStyle)
+            let containsStrikethrough = attributes.keys.contains(.strikethroughStyle)
+            if containsUnderline {
+                attributes[.underlineColor] = color
+            }
+            if containsStrikethrough {
+                attributes[.strikethroughColor] = color
+            }
+            if !containsUnderline && !containsStrikethrough {
+                pendingColor = color
+            }
+        }
+
+        attributes = value.split(separator: " ").reduce(into: attributes) {
+            switch $1 {
+            // Line
+            case "underline":    applyLine(.underlineStyle, .underlineColor, attributes: &$0)
+            case "line-through": applyLine(.strikethroughStyle, .strikethroughColor, attributes: &$0)
+            // Style
+            case "dotted": applyStyle(.patternDot, attributes: &$0)
+            case "solid":  applyStyle(.single, attributes: &$0)
+            case "double": applyStyle(.double, attributes: &$0)
+            case "dashed": applyStyle(.patternDash, attributes: &$0)
+            default:
+                // Color
+                if let color = Color(cssStyle: String($1)) {
+                    applyColor(color, attributes: &$0)
+                }
+            }
         }
     }
 }
